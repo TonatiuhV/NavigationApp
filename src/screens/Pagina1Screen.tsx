977a59d1dc390f817f109ebd79201a7831b7113c
= <=>
2022-06-14 18:12:05 -0500

    nvigation entre pantallas

--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StackScreenProps } from '@react-navigation/stack'
+import { Button, Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
 
-export const Pagina1Screen = () => {
+interface Props extends StackScreenProps<any, any>{}
+
+export const Pagina1Screen = ({navigation}:Props) => {
   return (
-    <View>
+    <View style={styles.globaMargin}>
         <Text>Pagina1Screen</Text>
+        <Button
+          title='Pagina 2'
+          onPress={() => navigation.navigate('Pagina2Screen')}
+        />
     </View>
   )
 }
--- src/screens/Pagina2Screen.tsx
+++ src/screens/Pagina2Screen.tsx
@@ -1,10 +1,19 @@
+
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Button, NativeEventEmitter, Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
 
 export const Pagina2Screen = () => {
+  const navigator = useNavigation()
   return (
-    <View>
+    <View style={styles.globaMargin}>
         <Text>Pagina2Screen</Text>
+        <Button
+        
+          title='Ir pagína 3'
+          onPress={ () => navigator.navigate("Pagina3Screen")} 
+        />
     </View>
   )
 }
