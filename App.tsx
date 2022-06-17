a9769480acac31a4e9d25d7c0d49f85c30fb35b9
= <=>
2022-06-16 22:20:47 -0500

    Configurcion Drawer Basico

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import 'react-native-gesture-handler';
+
 import React from 'react'
 //Actaizar de acuerdo a la documentacion
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './src/navigator/StackNavigator';
+// import { StackNavigator } from './src/navigator/StackNavigator';
+import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
 
 const App = () => {
   return (
     <NavigationContainer>
       
-      <StackNavigator/>
+      {/* <StackNavigator/> */}
+      <MenuLateralBasico/>
     </NavigationContainer>
   )
 }
