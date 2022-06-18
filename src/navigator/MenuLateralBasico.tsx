22a242735d7cdc109164a16b150ed4a85843894b
= <=>
2022-06-17 20:40:33 -0500

    toogle Drawer

--- src/navigator/MenuLateralBasico.tsx
+++ src/navigator/MenuLateralBasico.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StackNavigator } from './StackNavigator';
 import { SettingsScreen } from '../screens/SettingsScreen';
+import { useWindowDimensions } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export const MenuLateralBasico = () => {
+  const { width }= useWindowDimensions()
   return (
     <Drawer.Navigator
+      
       screenOptions={{
-        headerShown:false
+        headerShown:false,
+        drawerType: (width) >= 658 ? 'permanent': 'front',
       }}  
     >
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StackScreenProps } from '@react-navigation/stack'
 import { Button, Text, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import { DrawerScreenProps } from '@react-navigation/drawer'
 
-interface Props extends StackScreenProps<any, any>{}
+// interface Props extends StackScreenProps<any, any>{}
+interface Props extends DrawerScreenProps<any, any>{}
 
 export const Pagina1Screen = ({navigation}:Props) => {
+  
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft:  () =>{
+        return (<Button
+          title='Menú'
+          onPress={() => {navigation.toggleDrawer() }}
+        />)
+      }
+    })
+    
+  }, [])
+  
+  
+  
   return (
     <View style={styles.globaMargin}>
         <Text style={styles.title}>Pagina1Screen</Text>
